4f50b22163ebf4312b104fc014b9253765e4ce8a
sweta56 <swetagurung72@gmail.com>
2022-07-30 00:15:24 +0545

    first commit

---
+++ femini/src/components/Index.js
@@ -0,0 +1,9 @@
+import Home from './Home.jsx';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import About from '../pages/About';
+import Calendar from '../pages/Calendar';
+import Appointment from '../pages/Appointment.jsx';
+
+
+export {Home,Login,Signup,About,Calendar,Appointment};
